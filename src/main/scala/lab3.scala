0e735b1fa9412dde310b20cf10227b19f8c56c42
Aveletsky <ixus.van.axel@gmail.com>
2016-11-16 01:23:58 +0300

    update lab3

--- src/main/scala/lab3.scala
+++ src/main/scala/lab3.scala
@@ -1,31 +1,31 @@
 package main.scala
 
 object lab3 {
-  def main(args: Array[String]): Unit = {
-
-    def printTree(tree: Tree): Unit = {
-      if(tree != null)
-        printTree(tree.getLeftSubtree)
-        println(tree.getNodeData)
-        printTree(tree.getRightSubtree)
+  object binaryTreeOps{
+    def printTree(tree: Tree, space: String = "|"): Unit = {
+      if (tree != null) {
+        printTree(tree.getRightSubtree, space + "-")
+        println(space + tree.getNodeData)
+        printTree(tree.getLeftSubtree, space + "-")
+      }
     }
 
     def insert(nodeData: Int, tree: Tree): Tree =  {
       if (tree == null) Leaf(nodeData)
       else
-        if (nodeData <= tree.getNodeData)
-          Node(insert(nodeData, tree.getLeftSubtree), tree.getRightSubtree, tree.getNodeData)
-        else
-          Node(tree.getLeftSubtree, insert(nodeData, tree.getRightSubtree), tree.getNodeData)
+      if (nodeData <= tree.getNodeData)
+        Node(insert(nodeData, tree.getLeftSubtree), tree.getRightSubtree, tree.getNodeData)
+      else
+        Node(tree.getLeftSubtree, insert(nodeData, tree.getRightSubtree), tree.getNodeData)
     }
 
     def contains(nodeDate: Int, tree: Tree): Boolean = {
       if (tree == null) false
       else
-        if (nodeDate == tree.getNodeData) true
-        else
-          if (nodeDate < tree.getNodeData) contains(nodeDate, tree.getLeftSubtree)
-          else contains(nodeDate, tree.getRightSubtree)
+      if (nodeDate == tree.getNodeData) true
+      else
+      if (nodeDate < tree.getNodeData) contains(nodeDate, tree.getLeftSubtree)
+      else contains(nodeDate, tree.getRightSubtree)
     }
 
     def sum(tree: Tree): Int = {
@@ -34,5 +34,18 @@ object lab3 {
     }
   }
 
+  def main(args: Array[String]): Unit = {
+    val r = scala.util.Random
+    var tree: Tree = Leaf(50)
 
+    for(a <- 1 to 14){tree = binaryTreeOps.insert(r.nextInt(100), tree)}
+    binaryTreeOps.printTree(tree)
+
+    Array(50,23).foreach( x => {
+      if(binaryTreeOps.contains(x, tree)){println(s"* ${x} присутвует в дереве")}
+      else{println(s"* ${x} не присутвует в дереве")}
+    })
+
+    println(s"Сумма элемента дерева ${binaryTreeOps.sum(tree)}")
+  }
 }
